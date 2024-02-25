123acba64bc9ffcf115a417d9a4e92dc89833236
teli203 <teli203@aol.com>
2024-02-25 14:12:54 -0600

    add .js file & src it in html

---
+++ pokemon.js
@@ -0,0 +1,8 @@
+const MAX_POKEMON = 151;                    // the max amount of pokemon's to retrieve //
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let
