85e096f715ef5572575ecd7d2da6486a3274f4c4
teli203 <teli203@aol.com>
2024-02-25 23:52:54 -0600

    add async func w/ promise results and catch error msg

--- pokemon.js
+++ pokemon.js
@@ -15,6 +15,16 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)     // first: fe
 
 async function fetchPokemonDatabeforeRedirect(id) {
     try {
-        
+        const [pokemon, pokemonSpecies] = await Promise.all
+        ([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => 
+            res.json()
+        ),
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => 
+            res.json()
+        ),
+    ])
+    return true
+    } catch (error) {
+        console.error("Failed to fetch Pokemon data before redirect");
     }
 }
