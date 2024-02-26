bcbbcd0877f1284869a11249237397530f3fa828
teli203 <teli203@aol.com>
2024-02-25 23:42:48 -0600

    add api fetch for pokemons

--- pokemon.js
+++ pokemon.js
@@ -5,4 +5,16 @@ const numberFilter = document.querySelector("#number");
 const nameFilter = document.querySelector("#name");
 const notFoundMessage = document.querySelector("#not-found-message");
 
-let
+let allPokemons = [];                      // empty array to store all the pokemons //
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)     // first: fetching the pokemons
+.then((response) => response.json())                                // then you receive the obj. w/ an array of 151 results for pokemons //
+.then((data) => {                                                   // then you take that data //
+    allPokemons = data.results;
+});
+
+async function fetchPokemonDatabeforeRedirect(id) {
+    try {
+        
+    }
+}
