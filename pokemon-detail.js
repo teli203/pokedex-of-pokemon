b8080f274afbc14ad470625cd439fc6b4b9f46a4
teli203 <teli203@aol.com>
2024-02-28 00:16:04 -0600

    adjust code & add color

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -1,62 +1,89 @@
 let currentPokemonId = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-    const MAX_POKEMONS = 151;
-    const pokemonID = new URLSearchParams(window.location.search).get("id");
-    const id = parseInt(pokemonID, 10);
+  const MAX_POKEMONS = 151;
+  const pokemonID = new URLSearchParams(window.location.search).get("id");
+  const id = parseInt(pokemonID, 10);
 
-    if (id < 1 || id > MAX_POKEMONS) {
-        return (window.location.href = "./index.html");
-    }
+  if (id < 1 || id > MAX_POKEMONS) {
+    return (window.location.href = "./index.html");
+  }
 
-    currentPokemonId = id;
-    loadPokemon(id);
+  currentPokemonId = id;
+  loadPokemon(id);
 });
 
 async function loadPokemon(id) {
-    try {
-        const [pokemon, pokemonSpecies] = await Promise.all([
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-              res.json()
-            ),
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
-              res.json()
-            ),
-          ]);
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
 
-          const abilitiesWrapper = document.querySelector("pokemon-detail-wrap .pokemon-detail.move");
-          abilitiesWrapper.innerHTML = "";
+    const abilitiesWrapper = document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail.move"
+    );
+    abilitiesWrapper.innerHTML = "";
 
-          if (currentPokemonId === id) {
-            displayPokemonDetails(pokemon);
-                const flavorText = getEnglishFlavorText(pokemonSpecies);
-                document.querySelector("./body3-fonts.pokemon-description").textContent = flavorText;
+    if (currentPokemonId === id) {
+      displayPokemonDetails(pokemon);
+      const flavorText = getEnglishFlavorText(pokemonSpecies);
+      document.querySelector(".body3-fonts.pokemon-description").textContent =
+        flavorText;
 
-                const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) => 
-                    document.querySelector(sel)
-                );
-                leftArrow.removeEventListener("click", navigatePokemon);
-                rightArrow.removeEventListener("click", navigatePokemon);
+      const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) =>
+        document.querySelector(sel)
+      );
+      leftArrow.removeEventListener("click", navigatePokemon);
+      rightArrow.removeEventListener("click", navigatePokemon);
 
-                if(id !== 1) {
-                    leftArrow.addEventListener("clickl", () => {
-                        navigatePokemon(id - 1);
-                    });
-                }
-            }
-                if(id !== 151) {
-                    rightArrow.addEventListener("clickl", () => {
-                        navigatePokemon(id + 1);
-                    });
-                }
+      if (id !== 1) {
+        leftArrow.addEventListener("click", () => {
+          navigatePokemon(id - 1);
+        });
+      }
+      if (id !== 151) {
+        rightArrow.addEventListener("click", () => {
+          navigatePokemon(id + 1);
+        });
+      }
 
-                window.history.pushState({}, "", `./detail.html?id=${id}`)
-            }
-          
-
-        return true;
-    }catch (error) {
-        console.error("An error occured while fetching Pokemon data:", error);
-        return false;
+      window.history.pushState({}, "", `./detail.html?id=${id}`);
     }
+
+    return true;
+  } catch (error) {
+    console.error("An error occured while fetching Pokemon data:", error);
+    return false;
+  }
 }
+
+async function navigatePokemon(id) {
+  currentPokemonId = id;
+  await loadPokemon(id);
+}
+
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  dark: "#EE99AC",
+};
