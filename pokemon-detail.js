d8e8e1aeba80caa72f7234f6076449127e82ca4c
teli203 <teli203@aol.com>
2024-02-28 00:55:52 -0600

    add main img element/divide weight by 10kg/ add ability

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -150,3 +150,104 @@ function setElementStyles(elements, cssProperty, value) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   }
   
+  function createAndAppendElement(parent, tag, options = {}) {
+    const element = document.createElement(tag);
+    Object.keys(options).forEach((key) => {
+      element[key] = options[key];
+    });
+    parent.appendChild(element);
+    return element;
+  }
+  
+  function displayPokemonDetails(pokemon) {
+    const { name, id, types, weight, height, abilities, stats } = pokemon;
+    const capitalizePokemonName = capitalizeFirstLetter(name);
+  
+    document.querySelector("title").textContent = capitalizePokemonName;
+  
+    const detailMainElement = document.querySelector(".detail-main");
+    detailMainElement.classList.add(name.toLowerCase());
+  
+    document.querySelector(".name-wrap .name").textContent =
+      capitalizePokemonName;
+  
+    document.querySelector(
+      ".pokemon-id-wrap .body2-fonts"
+    ).textContent = `#${String(id).padStart(3, "0")}`;
+  
+    const imageElement = document.querySelector(".detail-img-wrapper img");
+    imageElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    imageElement.alt = name;
+  
+    const typeWrapper = document.querySelector(".power-wrapper");
+    typeWrapper.innerHTML = "";
+    types.forEach(({ type }) => {
+      createAndAppendElement(typeWrapper, "p", {
+        className: `body3-fonts type ${type.name}`,
+        textContent: type.name,
+      });
+    });
+  
+    document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail p.body3-fonts.weight"
+    ).textContent = `${weight / 10}kg`;
+    document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail p.body3-fonts.height"
+    ).textContent = `${height / 10}m`;
+  
+    const abilitiesWrapper = document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail.move"
+    );
+    abilities.forEach(({ ability }) => {
+      createAndAppendElement(abilitiesWrapper, "p", {
+        className: "body3-fonts",
+        textContent: ability.name,
+      });
+    });
+  
+    const statsWrapper = document.querySelector(".stats-wrapper");
+    statsWrapper.innerHTML = "";
+  
+    const statNameMapping = {
+      hp: "HP",
+      attack: "ATK",
+      defense: "DEF",
+      "special-attack": "SATK",
+      "special-defense": "SDEF",
+      speed: "SPD",
+    };
+  
+    stats.forEach(({ stat, base_stat }) => {
+      const statDiv = document.createElement("div");
+      statDiv.className = "stats-wrap";
+      statsWrapper.appendChild(statDiv);
+  
+      createAndAppendElement(statDiv, "p", {
+        className: "body3-fonts stats",
+        textContent: statNameMapping[stat.name],
+      });
+  
+      createAndAppendElement(statDiv, "p", {
+        className: "body3-fonts",
+        textContent: String(base_stat).padStart(3, "0"),
+      });
+  
+      createAndAppendElement(statDiv, "progress", {
+        className: "progress-bar",
+        value: base_stat,
+        max: 100,
+      });
+    });
+  
+    setTypeBackgroundColor(pokemon);
+  }
+  
+  function getEnglishFlavorText(pokemonSpecies) {
+    for (let entry of pokemonSpecies.flavor_text_entries) {
+      if (entry.language.name === "en") {
+        let flavor = entry.flavor_text.replace(/\f/g, " ");
+        return flavor;
+      }
+    }
+    return "";
+  }
