62f660a64d5f51f9471c4fdf1165c59804e0c10d
teli203 <teli203@aol.com>
2024-02-28 15:37:39 -0600

    add query selectors

---
+++ search.js
@@ -0,0 +1,3 @@
+const inputElement = document.querySelector("#search-input");
+const search_icon = document.querySelector("#search-close-icon");
+const sort_wrapper = document.querySelector(".sort-wrapper");
