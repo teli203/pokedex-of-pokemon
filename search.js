01c58cfcbb96aaa294e4ac5a545a5c0fac83163c
teli203 <teli203@aol.com>
2024-02-28 15:38:37 -0600

    add eventlisten / onclick

--- search.js
+++ search.js
@@ -1,3 +1,9 @@
 const inputElement = document.querySelector("#search-input");
 const search_icon = document.querySelector("#search-close-icon");
 const sort_wrapper = document.querySelector(".sort-wrapper");
+
+inputElement.addEventListener("input", () => {
+    handleInputChange(inputElement);
+  });
+  search_icon.addEventListener("click", handleSearchCloseOnClick);
+  sort_wrapper.addEventListener("click", handleSortIconOnClick);
