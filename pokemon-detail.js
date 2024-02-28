1f1b6a7a106118bfb2ea6b209e816ef38df28990
teli203 <teli203@aol.com>
2024-02-27 21:30:13 -0600

    add right/left arrow deets & push history function to make sinlgel pg w/o reload

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -14,8 +14,49 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function loadPokemon(id) {
-    try {}
-    catch (error) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise.all([
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+              res.json()
+            ),
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+              res.json()
+            ),
+          ]);
+
+          const abilitiesWrapper = document.querySelector("pokemon-detail-wrap .pokemon-detail.move");
+          abilitiesWrapper.innerHTML = "";
+
+          if (currentPokemonId === id) {
+            displayPokemonDetails(pokemon);
+                const flavorText = getEnglishFlavorText(pokemonSpecies);
+                document.querySelector("./body3-fonts.pokemon-description").textContent = flavorText;
+
+                const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((sel) => 
+                    document.querySelector(sel)
+                );
+                leftArrow.removeEventListener("click", navigatePokemon);
+                rightArrow.removeEventListener("click", navigatePokemon);
+
+                if(id !== 1) {
+                    leftArrow.addEventListener("clickl", () => {
+                        navigatePokemon(id - 1);
+                    });
+                }
+            }
+                if(id !== 151) {
+                    rightArrow.addEventListener("clickl", () => {
+                        navigatePokemon(id + 1);
+                    });
+                }
+
+                window.history.pushState({}, "", `./detail.html?id=${id}`)
+            }
+          
+
+        return true;
+    }catch (error) {
         console.error("An error occured while fetching Pokemon data:", error);
+        return false;
     }
 }
