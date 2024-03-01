d490f45f5a240020fb9880829e0b4d60bc1ef874
teli203 <teli203@aol.com>
2024-02-29 20:09:20 -0600

    add search.js & style.css work out errors for api and index.html

--- search.js
+++ search.js
@@ -7,3 +7,31 @@ inputElement.addEventListener("input", () => {
   });
   search_icon.addEventListener("click", handleSearchCloseOnClick);
   sort_wrapper.addEventListener("click", handleSortIconOnClick);
+
+  function handleInputChange(inputElement) {
+    const inputValue = inputElement.value;
+  
+    if (inputValue !== "") {
+      document
+        .querySelector("#search-close-icon")
+        .classList.add("search-close-icon-visible");
+    } else {
+      document
+        .querySelector("#search-close-icon")
+        .classList.remove("search-close-icon-visible");
+    }
+  }
+
+  function handleSearchCloseOnClick() {
+    document.querySelector("#search-input").value = "";
+    document
+      .querySelector("#search-close-icon")
+      .classList.remove("search-close-icon-visible");
+  }
+  
+  function handleSortIconOnClick() {
+    document
+      .querySelector(".filter-wrapper")
+      .classList.toggle("filter-wrapper-open");
+    document.querySelector("body").classList.toggle("filter-wrapper-overlay");
+  }
